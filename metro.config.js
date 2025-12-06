3d676e0ea9cacb99f487ee4cb16723a3eab11bc4
Ardian <ardiannn98@gmail.com>
2025-12-06 17:59:38 +0700

    update logo dan splash

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,9 @@ config.resolver.extraNodeModules = {
   buffer: require.resolve('buffer/'),
 };
 
+// ✅ TAMBAHKAN INI: Agar Metro bisa membaca file .cjs (Solusi Error Anda)
+config.resolver.sourceExts.push('cjs');
+
 config.resolver.assetExts.push(
   'tflite',  // TensorFlow Lite models
   'txt'      // Label files
